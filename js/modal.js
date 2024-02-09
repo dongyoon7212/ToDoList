7eac5573bf58829d44db86b3a01e802e435853d7
dongyoon7212 <dongyoon7212@naver.com>
2024-02-09 20:36:45 +0900

    todolist front web, javascript modal show/off

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+function handleClickedAddButton() {
+    const rootModal = document.querySelector(".root-modal");
+
+    rootModal.classList.add("modal-show");
+}
+
+function handleClickedCloseButton() {
+    const rootModal = document.querySelector(".root-modal");
+
+    rootModal.classList.remove("modal-show");
+}
+
+
